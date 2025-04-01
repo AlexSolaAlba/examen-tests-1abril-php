53ec02ec51bd617d03a70caaced10e0de24a117a
Alejandro Sola <alejandrosolaalba@gmail.com>
2025-04-01 10:33:50 +0200

    [#] - Test that given a string returns an empty string

--- src/Examen.php
+++ src/Examen.php
@@ -2,8 +2,19 @@
 
 namespace Deg540\ExamenTests1Abril;
 
+use Exception;
+
 class Examen
 {
     // TODO: Examen Kata
 
+    /**
+     * @param string $instruction
+     *
+     * @return string
+     */
+    public function returnBorrowedBooks(string $instruction): string
+    {
+        return "" ;
+    }
 }
--- tests/ExamenTest.php
+++ tests/ExamenTest.php
@@ -2,10 +2,30 @@
 
 namespace Deg540\ExamenTests1Abril\Test;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use Deg540\ExamenTests1Abril\Examen;
 
 class ExamenTest extends TestCase
 {
     // TODO: Examen Kata Tests
+
+    private Examen $examen;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->examen = new Examen();
+    }
+
+    /**
+     * @test
+     */
+    public function givenStringReturnsEmptyString() : void
+    {
+        $returnValue = $this->examen->returnBorrowedBooks("ale");
+        $this->assertEquals("",$returnValue);
+    }
+
+
 }
